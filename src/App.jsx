4593bb2ae1b25a7855f4c79ebc80ca6ed2e2de84
Rajat Arya <rajatarya466@gmail.com>
2026-07-24 13:44:27 +0530

    Rename login.jsx to Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ComingSoon from "./pages/ComingSoon";
 
 function App() {
   return (
     <BrowserRouter>
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/attendance" element={<ComingSoon />} />
+        <Route path="/payroll" element={<ComingSoon />} />
+        <Route path="/departments" element={<ComingSoon />} />
+        <Route path="/analytics" element={<ComingSoon />} />
+        <Route path="/settings" element={<ComingSoon />} />
+      </Routes>
     </BrowserRouter>
   );
 }
